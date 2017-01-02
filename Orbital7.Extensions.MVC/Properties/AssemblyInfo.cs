f856181e520bce689768bcae7b67c10d61cd268f
Josh van Eikeren <josh@vaneikeren.com>
2017-01-02 14:39:47 -0800

    Updated to 2017 copyrights

--- Orbital7.Extensions.MVC/Properties/AssemblyInfo.cs
+++ Orbital7.Extensions.MVC/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Orbital7.Extensions.MVC")]
-[assembly: AssemblyCopyright("Copyright (c) 2012-2016 Orbital7")]
+[assembly: AssemblyCopyright("Copyright (c) 2012-2017 Orbital7")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
--- Orbital7.Extensions/Properties/AssemblyInfo.cs
+++ Orbital7.Extensions/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Orbital7.Extensions")]
-[assembly: AssemblyCopyright("Copyright (c) 2012-2016 Orbital7")]
+[assembly: AssemblyCopyright("Copyright (c) 2012-2017 Orbital7")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
