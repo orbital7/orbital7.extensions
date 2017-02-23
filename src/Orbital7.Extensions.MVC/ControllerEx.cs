cab3495cf3f020e2437208c0d62e5ec69cacf323
Josh van Eikeren <josh@vaneikeren.com>
2017-02-23 12:14:06 -0800

    Added GetUserID() method to ControllerEx

--- src/Orbital7.Extensions.MVC/ControllerEx.cs
+++ src/Orbital7.Extensions.MVC/ControllerEx.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
 
 namespace System.Web.Mvc
 {
@@ -49,6 +46,11 @@ namespace System.Web.Mvc
                 return postedFile.InputStream.ReadAll();
             else
                 return null;
+        }
+
+        public Guid GetUserID()
+        {
+            return Guid.Parse(this.User.Identity.GetUserId());
         }
     }
 }
