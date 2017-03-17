0c0a9b37587567d0f6b7cc2ca4b100450943150e
Josh van Eikeren <josh@vaneikeren.com>
2017-03-17 11:51:33 -0700

    Added AdditionalInformationAttribute

---
+++ src/Orbital7.Extensions.Attributes/AdditionalInformationAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orbital7.Extensions.Attributes
+{
+    public class AdditionalInformationAttribute : Attribute
+    {
+        public string Value { get; private set; }
+
+        public AdditionalInformationAttribute(string value)
+            : base()
+        {
+            this.Value = value;
+        }
+
+        public override string ToString()
+        {
+            return this.Value;
+        }
+    }
+}
