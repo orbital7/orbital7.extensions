4e87745711a7d76dbc109bd1811e85b19a637eaf
Josh van Eikeren <josh@vaneikeren.com>
2017-06-02 21:43:29 -0700

    Added Exception Extensions

---
+++ src/Orbital7.Extensions/ExceptionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class ExceptionExtensions
+    {
+        public static string FlattenMessages(this Exception ex, string delim = "; ")
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(ex.Message);
+
+            var innerEx = ex.InnerException;
+            while (innerEx != null)
+            {
+                sb.Append(delim);
+                sb.Append(innerEx.Message);
+                innerEx = innerEx.InnerException;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
