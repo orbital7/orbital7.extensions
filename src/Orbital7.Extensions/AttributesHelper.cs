cf18f869270123a7b0d491c32d5061381e3aef2d
Josh van Eikeren <josh@vaneikeren.com>
2017-08-18 14:45:42 -0700

    Added Attribute extensions

--- src/Orbital7.Extensions/AttributesHelper.cs
+++ src/Orbital7.Extensions/AttributesHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
 
@@ -32,6 +34,45 @@ namespace Orbital7.Extensions
                             if (info.CanRead && String.Compare(info.Name, attributePropertyName, StringComparison.CurrentCultureIgnoreCase) == 0)
                                 return info.GetValue(attributeInstance, null);
                 }
+            }
+
+            return null;
+        }
+
+        public static string GetPropertyDisplayName<T>(Expression<Func<T, object>> propertyExpression)
+        {
+            var memberInfo = GetPropertyInformation(propertyExpression.Body);
+            if (memberInfo == null)
+            {
+                throw new ArgumentException(
+                    "No property reference expression was found.",
+                    "propertyExpression");
+            }
+
+            var attr = memberInfo.GetAttribute<System.ComponentModel.DataAnnotations.DisplayAttribute>(false);
+            if (attr == null)
+            {
+                return memberInfo.Name;
+            }
+
+            return attr.Name;
+        }
+
+        public static MemberInfo GetPropertyInformation(Expression propertyExpression)
+        {
+            MemberExpression memberExpr = propertyExpression as MemberExpression;
+            if (memberExpr == null)
+            {
+                UnaryExpression unaryExpr = propertyExpression as UnaryExpression;
+                if (unaryExpr != null && unaryExpr.NodeType == ExpressionType.Convert)
+                {
+                    memberExpr = unaryExpr.Operand as MemberExpression;
+                }
+            }
+
+            if (memberExpr != null && memberExpr.Member.MemberType == MemberTypes.Property)
+            {
+                return memberExpr.Member;
             }
 
             return null;
---
+++ src/Orbital7.Extensions/ReflectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace System
+{
+    public static class ReflectionExtensions
+    {
+        public static T GetAttribute<T>(this MemberInfo member, bool isRequired)
+            where T : Attribute
+        {
+            var attribute = member.GetCustomAttributes(typeof(T), false).SingleOrDefault();
+
+            if (attribute == null && isRequired)
+            {
+                throw new ArgumentException(
+                    string.Format(
+                        CultureInfo.InvariantCulture,
+                        "The {0} attribute must be defined on member {1}",
+                        typeof(T).Name,
+                        member.Name));
+            }
+
+            return (T)attribute;
+        }
+    }
+}
