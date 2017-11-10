3cd1d5e75644ed19c8ea0bcadd0f9eb64fb7fd6d
Josh van Eikeren <josh@vaneikeren.com>
2017-11-09 19:44:33 -0800

    Added CC/BCC parameters to EmailEngine methods

--- src/Orbital7.Extensions/Email/EmailEngine.cs
+++ src/Orbital7.Extensions/Email/EmailEngine.cs
@@ -8,10 +8,11 @@ namespace Orbital7.Extensions.Email
     public abstract class EmailEngine
     {
         public void SendTextEmail(string fromAddress, string fromName, string toAddresses, string subject,
-            string textBody, bool sendAsync, IDictionary<string, string> textReplacementKeys = null, 
+            string textBody, bool sendAsync, string ccAddresses = null, string bccAddresses = null, 
+            IDictionary <string, string> textReplacementKeys = null, 
             IList<string> attachmentFilePaths = null, IDictionary<string, byte[]> attachmentFileContents = null)
         {
-            Send(CreateMessage(fromAddress, fromName, toAddresses, subject)
+            Send(CreateMessage(fromAddress, fromName, toAddresses, subject, ccAddresses, bccAddresses)
                  .SetTextBody(textBody, textReplacementKeys)
                  .AttachFiles(attachmentFilePaths)
                  .AttachFiles(attachmentFileContents),
@@ -19,11 +20,12 @@ namespace Orbital7.Extensions.Email
         }
 
         public void SendHtmlEmail(string fromAddress, string fromName, string toAddresses, string subject,
-            string htmlBody, bool sendAsync, IDictionary<string, string> textReplacementKeys = null, 
+            string htmlBody, bool sendAsync, string ccAddresses = null, string bccAddresses = null, 
+            IDictionary <string, string> textReplacementKeys = null, 
             IDictionary<string, byte[]> imageContentReplacementKeys = null, string imageContentType = "image/png", 
             IList<string> attachmentFilePaths = null, IDictionary<string, byte[]> attachmentFileContents = null)
         {
-            Send(CreateMessage(fromAddress, fromName, toAddresses, subject)
+            Send(CreateMessage(fromAddress, fromName, toAddresses, subject, ccAddresses, bccAddresses)
                  .SetHtmlBody(htmlBody, textReplacementKeys, imageContentReplacementKeys, imageContentType)
                  .AttachFiles(attachmentFilePaths)
                  .AttachFiles(attachmentFileContents),
@@ -31,11 +33,12 @@ namespace Orbital7.Extensions.Email
         }
 
         public void SendHtmlEmail(string fromAddress, string fromName, string toAddresses, string subject,
-            string htmlBody, bool sendAsync, IDictionary<string, string> textReplacementKeys = null, 
+            string htmlBody, bool sendAsync, string ccAddresses = null, string bccAddresses = null, 
+            IDictionary <string, string> textReplacementKeys = null, 
             IDictionary<string, string> imageFilePathReplacementKeys = null, string imageContentType = "image/png", 
             IList<string> attachmentFilePaths = null, IDictionary<string, byte[]> attachmentFileContents = null)
         {
-            Send(CreateMessage(fromAddress, fromName, toAddresses, subject)
+            Send(CreateMessage(fromAddress, fromName, toAddresses, subject, ccAddresses, bccAddresses)
                  .SetHtmlBody(htmlBody, textReplacementKeys, imageFilePathReplacementKeys, imageContentType)
                  .AttachFiles(attachmentFilePaths)
                  .AttachFiles(attachmentFileContents),
@@ -44,17 +47,28 @@ namespace Orbital7.Extensions.Email
 
         protected abstract void Send(MailMessage message, bool sendAsync);
 
-        public static MailMessage CreateMessage(string fromAddress, string fromName, string toAddresses, string subject)
+        public static MailMessage CreateMessage(string fromAddress, string fromName, string toAddresses, string subject, 
+            string ccAddresses = null, string bccAddresses = null)
         {
             // Create the message.
             var mailMsg = new MailMessage();
 
-            // To.
-            foreach (string toAddress in toAddresses.Parse(";"))
-                mailMsg.To.Add(new MailAddress(toAddress.Trim()));
-
             // From.
             mailMsg.From = new MailAddress(fromAddress.Trim(), fromName.Trim());
+
+            // To.
+            foreach (string address in toAddresses.Parse(";"))
+                mailMsg.To.Add(new MailAddress(address.Trim()));
+
+            // CC.
+            if (!String.IsNullOrEmpty(ccAddresses))
+                foreach (string address in ccAddresses.Parse(";"))
+                    mailMsg.CC.Add(new MailAddress(address.Trim()));
+
+            // BCC.
+            if (!String.IsNullOrEmpty(bccAddresses))
+                foreach (string address in bccAddresses.Parse(";"))
+                    mailMsg.Bcc.Add(new MailAddress(address.Trim()));
 
             // Subject.
             mailMsg.Subject = subject;
