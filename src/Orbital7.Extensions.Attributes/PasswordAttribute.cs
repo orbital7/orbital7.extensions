ecdaa9a7b6ef4c2066c3e71039638cc3ab9f5ce4
Josh van Eikeren <josh@vaneikeren.com>
2018-02-21 17:07:00 -0800

    Added PasswordAttribute

---
+++ src/Orbital7.Extensions.Attributes/PasswordAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orbital7.Extensions.Attributes
+{
+    public class PasswordAttribute : DataTypeAttribute
+    {
+        public PasswordAttribute()
+            : base(DataType.Password)
+        {
+            
+        }
+    }
+}
