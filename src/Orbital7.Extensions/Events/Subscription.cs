c3a6acc763a3b09d939bc36fa1729cb8f86a3994
Josh van Eikeren <josh@vaneikeren.com>
2019-05-03 13:17:10 -0700

    Added Event/MVVM/Configuration features; moved .NET Framework assemblies to separate solution

---
+++ src/Orbital7.Extensions/Events/Subscription.cs
@@ -0,0 +1,19 @@
+﻿// Source: https://github.com/brminnick/AsyncAwaitBestPractices
+
+using System;
+using System.Reflection;
+
+namespace Orbital7.Extensions.Events
+{
+    internal struct Subscription
+    {
+        public Subscription(WeakReference subscriber, MethodInfo handler)
+        {
+            Subscriber = subscriber;
+            Handler = handler ?? throw new ArgumentNullException(nameof(handler));
+        }
+
+        public WeakReference Subscriber { get; }
+        public MethodInfo Handler { get; }
+    }
+}
