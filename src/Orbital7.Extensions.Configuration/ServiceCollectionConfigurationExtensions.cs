1d9867c0cfc9b4e69e29efa2c077271b5cd1c077
Josh van Eikeren <jvaneikeren@users.noreply.github.com>
2020-01-15 16:56:04 -0800

    Dependency updates, extension method consolidation

---
+++ src/Orbital7.Extensions.Configuration/ServiceCollectionConfigurationExtensions.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionConfigurationExtensions
+    {
+        public static IServiceCollection AddAppSettingsConfiguration(
+            this IServiceCollection services,
+            string environmentVariableName = "ASPNETCORE_ENVIRONMENT")
+        {
+            var builder = CreateConfigurationBuilder(environmentVariableName)
+                    .AddEnvironmentVariables();
+            services.AddSingleton<IConfiguration>(builder.Build());
+
+            return services;
+        }
+
+        public static IServiceCollection AddAppSettingsConfigurationWithUserSecrets<TStartup>(
+            this IServiceCollection services,
+            string environmentVariableName = "ASPNETCORE_ENVIRONMENT")
+            where TStartup : class
+        {
+            var builder = CreateConfigurationBuilder(environmentVariableName)
+                .AddUserSecrets<TStartup>()
+                .AddEnvironmentVariables();
+            services.AddSingleton<IConfiguration>(builder.Build());
+
+            return services;
+        }
+
+        private static IConfigurationBuilder CreateConfigurationBuilder(
+                string environmentVariableName = "ASPNETCORE_ENVIRONMENT")
+        {
+            var environment = Environment.GetEnvironmentVariable(environmentVariableName);
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true);
+
+            return builder;
+        }
+    }
+
+    //public static class ConfigurationBuilderFactory
+    //{
+    //    public static IServiceCollection AppendConfiguration(
+    //        IServiceCollection services,
+    //        string environmentVariableName = "ASPNETCORE_ENVIRONMENT")
+    //    {
+    //        var builder = CreateConfigurationBuilder(environmentVariableName)
+    //            .AddEnvironmentVariables();
+    //        services.AddSingleton<IConfiguration>(builder.Build());
+
+    //        return services;
+    //    }
+
+    //    public static IServiceCollection AppendConfigurationWithUserSecrets<TStartup>(
+    //        IServiceCollection services,
+    //        string environmentVariableName = "ASPNETCORE_ENVIRONMENT")
+    //        where TStartup : class
+    //    {
+    //        var builder = CreateConfigurationBuilder(environmentVariableName)
+    //            .AddUserSecrets<TStartup>()
+    //            .AddEnvironmentVariables();
+    //        services.AddSingleton<IConfiguration>(builder.Build());
+
+    //        return services;
+    //    }
+
+    //    public static IConfigurationBuilder CreateConfigurationBuilder(
+    //        string environmentVariableName = "ASPNETCORE_ENVIRONMENT")
+    //    {
+    //        var environment = Environment.GetEnvironmentVariable(environmentVariableName);
+    //        var builder = new ConfigurationBuilder()
+    //            .SetBasePath(Directory.GetCurrentDirectory())
+    //            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    //            .AddJsonFile($"appsettings.{environment}.json", optional: true);
+
+    //        return builder;
+    //    }
+    //}
+}
