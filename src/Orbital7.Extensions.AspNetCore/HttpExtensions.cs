fdd3a778f1bafc390dc6a983f0e5425ee9c262da
Josh van Eikeren <jvaneikeren@users.noreply.github.com>
2020-07-15 11:04:45 -0700

    Added GetIPAddress HttpRequest extension method

--- src/Orbital7.Extensions.AspNetCore/HttpExtensions.cs
+++ src/Orbital7.Extensions.AspNetCore/HttpExtensions.cs
@@ -7,23 +7,38 @@ namespace Microsoft.AspNetCore.Http
 {
     public static class HttpExtensions
     {
-        public static string GetRelativeUri(this HttpRequest request)
+        public static string GetRelativeUri(
+            this HttpRequest request)
         {
             return string.Format("{0}{1}", request.Path, request.QueryString.ToUriComponent());
         }
 
-        public static string GetAbsoluteUri(this HttpRequest request)
+        public static string GetAbsoluteUri(
+            this HttpRequest request)
         {
             return string.Format("{0}://{1}{2}{3}", request.Scheme, request.Host, request.Path, 
                 request.QueryString.ToUriComponent());
         }
 
-        public static string GetBaseUrl(this HttpRequest request)
+        public static string GetBaseUrl(
+            this HttpRequest request)
         {
             return string.Format("{0}://{1}/", request.Scheme, request.Host);
         }
 
-        public static void SetFailureState(this HttpResponse response)
+        public static string GetIPAddress(
+            this HttpRequest request)
+        {
+            var ipAddress = request.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            if (ipAddress == "::1")
+                ipAddress = "127.0.0.1";
+
+            return ipAddress;
+        }
+
+        public static void SetFailureState(
+            this HttpResponse response)
         {
             response.StatusCode = (int)HttpStatusCode.BadRequest;
         }
