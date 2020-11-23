ffd277f7739a5da1df939f4709d986c15f7b0f88
Josh van Eikeren <jvaneikeren@users.noreply.github.com>
2020-11-22 19:29:22 -0800

    Create VectorCurrencyConverter.cs

---
+++ src/Orbital7.Extensions.Xamarin/VectorCurrencyConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Xamarin.Forms
+{
+    public class VectorCurrencyConverter : IValueConverter
+    {
+        public object Convert(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var d = decimal.Parse(value.ToString());
+                var conversion = d.ToString("C");
+                if (d > 0)
+                    conversion = "+" + conversion;
+                return conversion;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            string valueFromString = Regex.Replace(value.ToString(), @"\D", "");
+
+            if (valueFromString.Length <= 0)
+                return null;
+
+            if (!long.TryParse(valueFromString, out long valueLong))
+                return null;
+
+            if (valueLong < 0)
+                return null;
+
+            return valueLong / 100m;
+        }
+    }
+}
