5035ad38b5f54e94b52edc7c4780aa99c33599b3
Josh van Eikeren <jvaneikeren@users.noreply.github.com>
2021-02-25 17:05:01 -0800

    Bug fixes and extensions

--- src/Orbital7.Extensions.AspNetCore/SerializableTupleExtensions.cs
+++ src/Orbital7.Extensions.AspNetCore/SerializableTupleExtensions.cs
@@ -8,12 +8,15 @@ namespace Microsoft.AspNetCore.Mvc
 {
     public static class SerializableTupleExtensions
     {
-        public static SelectList ToSelectList<T1, T2>(this IEnumerable<SerializableTuple<T1, T2>> list)
+        public static SelectList ToSelectList<T1, T2>(
+            this IEnumerable<SerializableTuple<T1, T2>> list)
         {
             return new SelectList(list, "Item1", "Item2");
         }
 
-        public static SelectList ToSelectList<T1, T2>(this IEnumerable<SerializableTuple<T1, T2>> list, T1 selected)
+        public static SelectList ToSelectList<T1, T2>(
+            this IEnumerable<SerializableTuple<T1, T2>> list, 
+            T1 selected)
         {
             return new SelectList(list, "Item1", "Item2", selected);
         }
