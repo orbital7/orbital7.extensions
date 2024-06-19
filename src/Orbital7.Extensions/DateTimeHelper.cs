16ded76c2e2abdcee336b241b6cf76cfb29ccac3
Josh van Eikeren <jvaneikeren@users.noreply.github.com>
2024-06-19 11:27:21 -0700

    Add IsReadOnly and Button support to RAEditor

--- src/Orbital7.Extensions/DateTimeHelper.cs
+++ src/Orbital7.Extensions/DateTimeHelper.cs
@@ -4,7 +4,8 @@ public static class DateTimeHelper
 {
     public const string DEFAULT_DATE_FORMAT = "MM/dd/yyyy";
 
-    public static TimeZoneInfo GetTimeZone(string timeZoneId)
+    public static TimeZoneInfo GetTimeZone(
+        string timeZoneId)
     {
         if (!string.IsNullOrEmpty(timeZoneId))
             return TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
