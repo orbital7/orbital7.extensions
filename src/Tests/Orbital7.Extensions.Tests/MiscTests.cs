53feb7967f646f911786bfbb4d0fae8003fa5906
Josh van Eikeren <jvaneikeren@users.noreply.github.com>
2026-06-28 10:10:56 -0700

    Add MiscTests

---
+++ src/Tests/Orbital7.Extensions.Tests/MiscTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Orbital7.Extensions.Tests;
+
+public class MiscTests
+{
+    [Fact]
+    public void EqualityOperatorWithNullableDecimals()
+    {
+        decimal a = 0;
+        decimal? b = 0;
+        decimal? c = null;
+
+        Assert.True(a == b);
+        Assert.False(a == c);
+        Assert.False(b == c);
+        Assert.True(c == null);
+        Assert.False(c == 0);
+
+        Assert.True(b != c);
+        Assert.True(c != 0);
+    }
+}
